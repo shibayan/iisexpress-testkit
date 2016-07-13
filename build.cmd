50a1b30701832f922c4d762c3de47a41b31b2816
shibayan <shiba-yan@outlook.com>
2016-07-13 14:25:06 +0900

    ソリューション構成を変更

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@ SET PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin
 rd /Q .\output
 mkdir .\output
 
-msbuild .\src\IisExpressTestKit.sln /p:Configuration=Release
+msbuild .\IisExpressTestKit.sln /p:Configuration=Release
 .\tools\nuget.exe pack .\src\IisExpressTestKit\IisExpressTestKit.csproj -Prop Configuration=Release -BasePath .\src -OutputDirectory .\output
